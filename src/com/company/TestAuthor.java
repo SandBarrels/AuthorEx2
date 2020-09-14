3f62dd3611d87c070a13916c8668794e6196c46f
SandBarrels <70902509+SandBarrels@users.noreply.github.com>
2020-09-14 22:34:48 +0300

    Init

---
+++ src/com/company/TestAuthor.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TestAuthor {
+    public static void main(String[] args ){
+        Author author1 = new Author("Jack", "dgfdfd@gmail.com", 'm');
+        System.out.println(author1.toString());
+    }
+}
